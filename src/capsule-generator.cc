a2f5a12dcc6bc6d0b228c7567f2f80cd89163f4b
arindamrc <arindam.rc.08@gmail.com>
2017-01-27 12:32:46 +0100

    generating capsules from mesh files

--- src/capsule-generator.cc
+++ src/capsule-generator.cc
@@ -23,138 +23,315 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <algorithm>
+#include <iterator>
+#include <cstdlib>
+#include <sstream>
+#include <stdexcept>
+#include <regex>
 
 #include <boost/program_options.hpp>
+#include <boost/regex.hpp>
 
 #include <roboptim/capsule/fitter.hh>
 #include <roboptim/capsule/util.hh>
 
+#include <roboptim/capsule/pugiconfig.hpp>
+#include <roboptim/capsule/pugixml.hpp>
+
+#include "pugixml.cpp"
+
 using namespace roboptim;
 using namespace roboptim::capsule;
 
-int main(int argc, char** argv)
-{
-  try
-    {
-      namespace po = boost::program_options;
+//regex::::      \${side\*\([-+]?([0-9]*\.[0-9]+|[0-9]+)\)\}
 
-      po::options_description desc ("Options");
-      desc.add_options ()
-	("help", "Print this help and exit")
-	("solver", po::value<std::string> (), "Nonlinear solver used")
-	("log-dir", po::value<std::string> (), "Path to optimization logs")
-	("points", po::value<std::vector<double> > ()->multitoken ()->required (),
-	 "Points that will be encapsulated");
+const double side = 1;
 
-      po::positional_options_description positionalOptions;
+std::vector<double> getPoints(const char* filename) {
+	std::vector<double> points;
+	pugi::xml_document doc;
+	pugi::xml_parse_result result = doc.load_file(filename);
+	std::cout << "Load meshfile: " << result.description()
+			<< ", meshfile child name: " << doc.first_child().name()
+			<< std::endl;
 
-      po::variables_map vm;
+	const std::string path =
+			"/COLLADA/library_geometries/geometry/mesh/source[*]/float_array[contains(@id,'positions')]";
+	const pugi::xpath_node_set nodes = doc.select_nodes(path.c_str());
 
-      try
-	{
-	  po::store (po::command_line_parser (argc, argv)
-		     .options (desc)
-		     .positional (positionalOptions)
-		     .style(
-			    po::command_line_style::unix_style
-			    ^ po::command_line_style::allow_short
-			    )
-		     .run (),
-		     vm);
-
-	  // Display help message
-	  if (vm.count ("help"))
-	    {
-	      std::cout << desc;
-	      return EXIT_SUCCESS;
-	    }
-
-	  std::string solver = "ipopt";
-
-	  // Load (optional) NLP solver
-	  if (vm.count ("solver"))
-	    {
-	      solver = vm["solver"].as<std::string> ();
-	    }
-
-	  // Check that points data was given
-	  if (!vm.count ("points"))
-	    {
-	      std::cerr << "Error: missing mandatory point data." << std::endl;
-	      return EXIT_FAILURE;
-	    }
-
-	  // Get points from CLI options
-	  const std::vector<double>&
-	    points = vm["points"].as<std::vector<double> > ();
-
-	  if (points.size ()%3 != 0)
-	    {
-	      std::cerr << "Error: points should be an array of 3D points, "
-			<< "e.g. x0 y0 z0 x1 y1 z1 etc." << std::endl;
-	      return EXIT_FAILURE;
-	    }
-
-	  // Load polyhedron
-	  polyhedron_t polyhedron;
-	  for (size_t i = 0; i < points.size (); i+=3)
-	    {
-	      point_t p (points[i], points[i+1], points[i+2]);
-	      polyhedron.push_back (p);
-	    }
-
-	  // Fitter expects a vector of polyhedrons
-	  polyhedrons_t polyhedrons;
-	  polyhedrons.push_back (polyhedron);
-
-	  // Create fitter
-	  Fitter fitter (polyhedrons, solver);
-
-	  // Load (optional) log directory
-	  if (vm.count ("log-dir"))
-	    {
-	      fitter.logDirectory () = vm["log-dir"].as<std::string> ();
-	    }
-
-	  // Compute initial guess
-	  point_t P0;
-	  point_t P1;
-	  value_type r = 0.;
-	  argument_t initParam (7);
-
-	  polyhedrons_t convexPolyhedrons;
-	  computeConvexPolyhedron (polyhedrons, convexPolyhedrons);
-	  computeBoundingCapsulePolyhedron (convexPolyhedrons, P0, P1, r);
-	  convertCapsuleToSolverParam (initParam, P0, P1, r);
-
-	  // Compute optimal capsule
-	  fitter.computeBestFitCapsule (initParam);
-
-	  // Display result
-	  std::cout << "Initial: " << fitter.initParam () << std::endl;
-	  std::cout << "Solution: " << fitter.solutionParam () << std::endl;
+	for (const pugi::xpath_node* it = nodes.begin(); it != nodes.end(); ++it) {
+		const char* vertexString = it->node().text().get();
+		std::istringstream vertexStream(vertexString);
+		double point;
+		while (vertexStream >> point) {
+			points.push_back(point);
+		}
 	}
-      catch (boost::program_options::required_option& e)
-	{
-	  std::cerr << "Error: " << e.what() << std::endl << std::endl;
-
-	  return EXIT_FAILURE;
-	}
-      catch (boost::program_options::error& e)
-	{
-	  std::cerr << "Error: " << e.what() << std::endl << std::endl;
-
-	  return EXIT_FAILURE;
-	}
-
-    }
-  catch (std::exception& e)
-    {
-      std::cerr << "Unhandled Exception reached the top of main: "
-		<< e.what() << ", application will now exit" << std::endl;
-      return EXIT_FAILURE;
-    }
-
-  return EXIT_SUCCESS;
+	return points;
 }
+
+Fitter getCapsuleParams(std::vector<double> points) {
+	std::string solver = "ipopt";
+
+	if (points.size() % 3 != 0) {
+		throw std::invalid_argument(
+				"Error: points should be an array of 3D points, e.g. x0 y0 z0 x1 y1 z1 etc.");
+	}
+
+	// Load polyhedron
+	polyhedron_t polyhedron;
+	for (size_t i = 0; i < points.size(); i += 3) {
+		point_t p(points[i], points[i + 1], points[i + 2]);
+		polyhedron.push_back(p);
+	}
+
+	std::cout << "polyhedron constructed" << std::endl;
+
+	// Fitter expects a vector of polyhedrons
+	polyhedrons_t polyhedrons;
+	polyhedrons.push_back(polyhedron);
+
+	// Create fitter
+	Fitter fitter(polyhedrons, solver);
+
+	fitter.logDirectory() = "log";
+	// Compute initial guess
+	point_t P0;
+	point_t P1;
+	value_type r = 0.;
+	argument_t initParam(7);
+
+	polyhedrons_t convexPolyhedrons;
+	computeConvexPolyhedron(polyhedrons, convexPolyhedrons);
+	std::cout << "convex polyhedron computed" << std::endl;
+	computeBoundingCapsulePolyhedron(convexPolyhedrons, P0, P1, r);
+	std::cout << "initial bounding capsule computed" << std::endl;
+	convertCapsuleToSolverParam(initParam, P0, P1, r);
+
+	// Compute optimal capsule
+	fitter.computeBestFitCapsule(initParam);
+	std::cout << "best capsule computed" << std::endl;
+
+	return fitter;
+}
+
+std::vector<double> parseComponents(const char* str) {
+	std::cout << "parsing components from: " << str << std::endl;
+	std::vector<double> components;
+	std::istringstream stream(str);
+	std::string ci;
+
+	boost::smatch mXacro, mValue;
+	boost::regex eXacro("\\${side\\*\\(([-+]?[0-9]*\.[0-9]+|[0-9]+)\\)}");
+	boost::regex eValue("[-+]?([0-9]*\.[0-9]+|[0-9]+)");
+
+	while (stream >> ci) {
+		std::cout << "ci: " << ci << std::endl;
+		if (boost::regex_search(ci, mXacro, eXacro)) {
+//			std::cout << "match: " << mXacro[0].str() << std::endl;
+//			std::cout << "g1: " << mXacro[1].str() << std::endl;
+			components.push_back(side * atof(mXacro[1].str().c_str()));
+		} else {
+			components.push_back(atof(ci.c_str()));
+		}
+	}
+
+//	std::cout << components << std::endl;
+	return components;
+}
+
+void makeCapsule(pugi::xml_node linkNode, std::vector<double> e1, std::vector<double> e2, double r,
+		const char* rpyStr, const char* xyzStr, const char* scaleStr) {
+
+	printf(
+			"raw: e1[0]: %lf, e1[1]: %lf, e1[2]: %lf, e2[0]: %lf, e2[1]: %lf, e2[2]: %lf, r: %lf\n",
+			e1[0], e1[1], e1[2], e2[0], e2[1], e2[2], r);
+
+	std::vector<double> rpy(3);
+	std::vector<double> xyz(3);
+	std::vector<double> scale(3);
+
+	if (strlen(scaleStr) > 0) {
+		scale = parseComponents(scaleStr);
+	} else {
+		scale[0] = 0.001;
+		scale[1] = 0.001;
+		scale[2] = 0.001;
+	}
+
+	if (strlen(rpyStr) > 0) {
+		rpy = parseComponents(rpyStr);
+	} else {
+		rpy[0] = 0;
+		rpy[1] = 0;
+		rpy[2] = 0;
+	}
+
+	if (strlen(xyzStr) > 0) {
+		xyz = parseComponents(xyzStr);
+	} else {
+		xyz[0] = 0;
+		xyz[1] = 0;
+		xyz[2] = 0;
+	}
+
+	e1[0] *= scale[0];
+	e1[1] *= scale[1];
+	e1[2] *= scale[2];
+
+	e2[0] *= scale[0];
+	e2[1] *= scale[1];
+	e2[2] *= scale[2];
+
+	r *= scale[0];
+
+	std::vector<double> len(3);
+	len[0] = fabs(e2[0] - e1[0]);
+	len[1] = fabs(e2[1] - e1[1]);
+	len[2] = fabs(e2[2] - e1[2]);
+
+	double cyl_l = pow((len[0] * len[0] + len[1] * len[1] + len[2] * len[2]), 0.5);
+
+	std::cout << "scale: " << scale << std::endl;
+	std::cout << "cyl len mag: " << cyl_l << std::endl;
+	std::cout << "cyl len: " << len << std::endl;
+
+	printf(
+				"scaled: e1[0]: %lf, e1[1]: %lf, e1[2]: %lf, e2[0]: %lf, e2[1]: %lf, e2[2]: %lf, r: %lf\n",
+				e1[0], e1[1], e1[2], e2[0], e2[1], e2[2], r);
+
+	std::cout << "constructing s1" << std::endl;
+	// The first sphere
+	pugi::xml_node s1Collision = linkNode.append_child("collision");
+	pugi::xml_node s1Origin = s1Collision.append_child("origin");
+	std::stringstream s1rpySS;
+	s1rpySS << rpy[0] << " " << rpy[1] << " " << rpy[2];
+	std::cout << "s1rpySS: " << s1rpySS.str() << std::endl;
+	s1Origin.append_attribute("rpy").set_value(s1rpySS.str().c_str());
+	std::stringstream s1xyzSS;
+	s1xyzSS << (xyz[0] + e1[0]) << " " << (xyz[1] + e1[1]) << " "
+			<< (xyz[2] + e1[2]);
+	std::cout << "s1xyzSS: " << s1xyzSS.str() << std::endl;
+	s1Origin.append_attribute("xyz").set_value(s1xyzSS.str().c_str());
+	pugi::xml_node s1Geom = s1Collision.append_child("geometry");
+	pugi::xml_node s1 = s1Geom.append_child("sphere");
+	pugi::xml_attribute rs1 = s1.append_attribute("radius");
+	rs1.set_value(r);
+
+	std::cout << "constructing cyl" << std::endl;
+	// The middle cylinder
+	pugi::xml_node crCollision = linkNode.append_child("collision");
+	pugi::xml_node crOrigin = crCollision.append_child("origin");
+	std::stringstream crrpySS;
+	crrpySS << rpy[0] << " " << fmod((rpy[1] + 1.5708), 3.14) << " " << rpy[2];
+	crOrigin.append_attribute("rpy").set_value(crrpySS.str().c_str());
+	std::stringstream crxyzSS;
+	crxyzSS << (xyz[0] + e1[0] + len[0] / 2) << " "
+			<< (xyz[1] + e1[1] + len[1] / 2) << " "
+			<< (xyz[2] + e1[2] + len[2] / 2);
+	std::cout << "crxyzSS: " << crxyzSS.str() << std::endl;
+	crOrigin.append_attribute("xyz").set_value(crxyzSS.str().c_str());
+	pugi::xml_node crGeom = crCollision.append_child("geometry");
+	pugi::xml_node cyl = crGeom.append_child("cylinder");
+	pugi::xml_attribute crl = cyl.append_attribute("length");
+	crl.set_value(cyl_l);
+	pugi::xml_attribute crr = cyl.append_attribute("radius");
+	crr.set_value(r);
+
+	std::cout << "constructing s2" << std::endl;
+	// The second sphere
+	pugi::xml_node s2Collision = linkNode.append_child("collision");
+	pugi::xml_node s2Origin = s2Collision.append_child("origin");
+	std::stringstream s2rpySS;
+	s2rpySS << rpy[0] << " " << rpy[1] << " " << rpy[2];
+	s2Origin.append_attribute("rpy").set_value(s1rpySS.str().c_str());
+	std::stringstream s2xyzSS;
+	s2xyzSS << (xyz[0] + e1[0] + len[0]) << " " << (xyz[1] + e1[1] + len[1])
+			<< " " << (xyz[2] + e1[2] + len[2]);
+	s2Origin.append_attribute("xyz").set_value(s2xyzSS.str().c_str());
+	pugi::xml_node s2Geom = s2Collision.append_child("geometry");
+	pugi::xml_node s2 = s2Geom.append_child("sphere");
+	pugi::xml_attribute s2r = s2.append_attribute("radius");
+	s2r.set_value(r);
+
+}
+
+int main(int argc, char** argv) {
+
+	try {
+		pugi::xml_document doc;
+		pugi::xml_parse_result result = doc.load_file("./src/svh.urdf.xacro");
+		std::cout << "Load result: " << result.description()
+				<< ", 1st child name: " << doc.first_child().name()
+				<< std::endl;
+
+		const std::string path =
+				"/robot/xacro:macro/link[*]/collision/geometry/mesh";
+		const pugi::xpath_node_set nodes = doc.select_nodes(path.c_str());
+
+		for (const pugi::xpath_node* it = nodes.begin(); it != nodes.end();
+				++it) {
+			pugi::xml_node meshNode = it->node();
+			pugi::xml_node collisionNode = meshNode.parent().parent();
+			pugi::xml_node linkNode = collisionNode.parent();
+
+			const char *filename = meshNode.attribute("filename").value();
+
+			std::cout << " Meshfile: " << filename << std::endl;
+
+			std::vector<double> points = getPoints(filename);
+			std::cout << points.size() << " points pushed!" << std::endl;
+
+			Fitter fitter = getCapsuleParams(points);
+			std::vector<double> e1(3), e2(3);
+			double radius;
+			std::cout << "Initial: " << fitter.initParam() << std::endl;
+			std::cout << "Solution: " << fitter.solutionParam() << std::endl;
+
+			// assign values to e1,e2 and radius
+			e1[0] = fitter.solutionParam()(0, 0);
+			e1[1] = fitter.solutionParam()(1, 0);
+			e1[2] = fitter.solutionParam()(2, 0);
+
+			std::cout << "e1: 0,1,2: " << e1[0] << "," << e1[1] << "," << e1[2] << std::endl;
+
+			e2[0] = fitter.solutionParam()(3, 0);
+			e2[1] = fitter.solutionParam()(4, 0);
+			e2[2] = fitter.solutionParam()(5, 0);
+
+			std::cout << "e2: 0,1,2: " << e2[0] << "," << e2[1] << "," << e2[2] << std::endl;
+
+			radius = fitter.solutionParam()(6, 0);
+
+			std::cout << "rad: " << radius << std::endl;
+
+//			std::cout << "link node: " << linkNode.name() << std::endl;
+//			std::cout << "collision node: " << collisionNode.name() << std::endl;
+			pugi::xml_node originNode = collisionNode.child("origin");
+			const char* rpyStr = originNode.attribute("rpy").value();
+			const char* xyzStr = originNode.attribute("xyz").value();
+			const char* scaleStr = "";
+			if (meshNode.attribute("scale")) {
+				scaleStr = meshNode.attribute("scale").value();
+			}
+
+			if (linkNode.remove_child("collision")) {
+				std::cout << "deleted old collision node!!" << std::endl;
+
+				makeCapsule(linkNode, e1, e2, radius, rpyStr, xyzStr, scaleStr);
+			}
+
+			std::cout << std::endl;
+		}
+		doc.save_file("./src/svh_new.urdf.xacro");
+	} catch (std::exception& e) {
+		std::cerr << "Unhandled Exception reached the top of main: " << e.what()
+				<< ", application will now exit" << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
+
